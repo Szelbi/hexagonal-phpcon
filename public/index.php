f68f797f34045bc8d1064c801e795552a0d6bde4
Dawid Gos <dawid.gos@evisa.express>
2024-10-24 21:16:49 +0200

    composer create-project symfony/skeleton:"7.1.*"

--- public/index.php
+++ public/index.php
@@ -1,5 +1,13 @@
 <?php
 
+use App\Kernel;
+
+require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+
 echo 'Siemaszko';
 
 phpinfo();
+
+return function (array $context) {
+    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+};
